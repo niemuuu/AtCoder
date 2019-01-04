a228a12ce8eed6b7be08b01af1242b14a30d2396
yorori <yorori.too@gmail.com>
2019-01-04 12:35:03 +0900

    fixing commented out

--- welcome-to-atcoder/welcome-to-atcoder.go
+++ welcome-to-atcoder/welcome-to-atcoder.go
@@ -1,31 +1,33 @@
-// package main
+/* https://atcoder.jp/contests/abs/tasks/practice_1 */
 
-// import (
-// 	"bufio"
-// 	"fmt"
-// 	"os"
-// 	"strconv"
-// 	"strings"
-// )
+package main
 
-// var sc = bufio.NewScanner(os.Stdin)
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
-// func readLine() string {
-// 	if !sc.Scan() {
-// 		return ""
-// 	}
-// 	return sc.Text()
-// }
+var sc = bufio.NewScanner(os.Stdin)
 
-// func main() {
-// 	t1 := readLine()
-// 	t2 := strings.Split(readLine(), " ")
-// 	t3 := readLine()
+func readLine() string {
+	if !sc.Scan() {
+		return ""
+	}
+	return sc.Text()
+}
 
-// 	n1, _ := strconv.Atoi(t1)
-// 	n2, _ := strconv.Atoi(t2[0])
-// 	n3, _ := strconv.Atoi(t2[1])
-// 	total := n1 + n2 + n3
+func main() {
+	t1 := readLine()
+	t2 := strings.Split(readLine(), " ")
+	t3 := readLine()
 
-// 	fmt.Printf("%d %s\n", total, t3)
-// }
+	n1, _ := strconv.Atoi(t1)
+	n2, _ := strconv.Atoi(t2[0])
+	n3, _ := strconv.Atoi(t2[1])
+	total := n1 + n2 + n3
+
+	fmt.Printf("%d %s\n", total, t3)
+}
